c21f9525fde9a2bbd01932b1dfb0bc105300353e
juarez1994 <122844980+juarez1994@users.noreply.github.com>
2023-01-17 03:44:38 +0100

    Add files via upload

---
+++ exercise2.py
@@ -0,0 +1,8 @@
+#2.Create a function that reverses a string. (i.e. Apple will result in elppA).
+
+def reverser(text):
+    reversedtext=text[::-1]
+    print(reversedtext)
+
+#test
+reverser("Apple")
